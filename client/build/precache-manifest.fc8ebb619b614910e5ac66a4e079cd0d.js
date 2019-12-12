8ac4fd11e5435354247a4c705283ade9df4dfdfa
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-12-12 09:45:18 +0100

    ready to deploy

---
+++ client/build/precache-manifest.fc8ebb619b614910e5ac66a4e079cd0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76853c767c2c6eaeac4a57256862e92f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f26a1edff4588792f1d0",
+    "url": "/static/css/main.5197febc.chunk.css"
+  },
+  {
+    "revision": "2dc43fdeb360cb23ccbf",
+    "url": "/static/js/2.54723535.chunk.js"
+  },
+  {
+    "revision": "f26a1edff4588792f1d0",
+    "url": "/static/js/main.543a9817.chunk.js"
+  },
+  {
+    "revision": "78bf16a0484ce77f0eaa",
+    "url": "/static/js/runtime-main.d6906fc7.js"
+  }
+]);
